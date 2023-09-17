6f772ec7629cba0973328f1442c04df07aab76c4
Samyukta Ramnath <samyuktaramnath@gmail.com>
2023-09-17 10:42:09 -0400

    use sigaction instead of signal for sigint

--- src/shellSignal.cpp
+++ src/shellSignal.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 
 void registerSignals(){
-    signal ( SIGINT, signalHandler); 
+    struct sigaction sa = {0};
+    sa.sa_handler = signalHandler;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGINT, &sa, NULL);
 }
+
 
 void signalHandler(int sig){
 
